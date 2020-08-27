770e938508ae6158d66c653e5f570feb939a33ad
Razvan Tamazlicariu <razvan.tamazlicariu@deventure.com>
2020-08-27 10:32:00 +0300

    exp 2 - removed maps init key

--- DigifidelPod.podspec
+++ DigifidelPod.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'DigifidelPod'
-  s.version          = '2.3.0.1'
+  s.version          = '2.3.0.2'
   s.summary          = 'The pod of Digifidel version 2.3.0'
 
 # This description is used to generate tags and improve search results.
--- DigifidelPod/Classes/MapView.swift
+++ DigifidelPod/Classes/MapView.swift
@@ -18,7 +18,7 @@ public class MapView: UIView {
     public override init(frame: CGRect) {
         super.init(frame: frame)
         
-       GMSServices.provideAPIKey(googleMapsApiKey)
+//       GMSServices.provideAPIKey(googleMapsApiKey)
                  let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
                 let mapView = GMSMapView.map(withFrame: frame, camera: camera)
                 addSubview(mapView)
